47804b22b0baf62767866e6c673918506e1cf2db
Hillel Coates <hillel.g.coates@gmail.com>
2019-04-26 18:49:10 -0700

    :art: Make Field not entirely abstract, and add Units for formatting

--- src/main/java/org/frc1778/freezylib/logging/Field.java
+++ src/main/java/org/frc1778/freezylib/logging/Field.java
@@ -1,14 +1,75 @@
 package org.frc1778.freezylib.logging;
 
-/** Field */
+import com.google.gson.annotations.Expose;
+
 public abstract class Field {
 
-  public abstract String getName();
+  @Expose private String name;
+  @Expose private String unit;
+  @Expose private String type;
 
-  public abstract String getFormat();
+  public enum Units {
+    UNITLESS("ul"),
+
+    // Time
+    HOUR("h"),
+    MINUTE("min"),
+    SECOND("s"),
+    MILLISECOND("ms"),
+    NANOSECOND("ns"),
+    HERTZ("Hz"),
+    // Distance
+    METRE("m"),
+    CENTIMETRE("cm"),
+    FOOT("ft"),
+    INCH("in"),
+    // Angle
+    RADIAN("rad"),
+    DEGREE("°"),
+    // Electricity
+    VOLT("V"),
+    OHM("Ω"),
+    AMPERE("A");
+
+    private final String unit;
+
+    private Units(String representation) {
+      unit = representation;
+    }
+
+    @Override
+    public String toString() {
+      return unit;
+    }
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setUnit(String unit) {
+    this.unit = unit;
+  }
+
+  public void setUnit(Units unit) {
+    this.unit = unit.toString();
+  }
+
+  public void setType(Class<?> type) {
+    this.type = type.getName();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getUnit() {
+    return unit;
+  }
+
+  public String getType() {
+    return type;
+  }
 
   public abstract String getValue();
-
-  @Override
-  public abstract String toString();
 }
