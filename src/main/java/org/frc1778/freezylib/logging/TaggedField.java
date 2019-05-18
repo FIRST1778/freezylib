0a0aff199358f22175cb5d151314ff0c25ca6772
Hillel Coates <hillel.g.coates@gmail.com>
2019-05-18 14:47:08 -0700

    :sparkles: Add TaggedField, which is similar to a SubscribedField, yet will only post it's value once, for labels or tags in a log file

---
+++ src/main/java/org/frc1778/freezylib/logging/TaggedField.java
@@ -0,0 +1,32 @@
+package org.frc1778.freezylib.logging;
+
+import org.frc1778.freezylib.util.Measurement.UnitBase;
+
+public class TaggedField extends Field {
+
+  private String value;
+  private boolean isNewValue;
+
+  public TaggedField(String name, String unit, Class<?> type) {
+    setName(name);
+    setUnit(unit);
+    this.value = String.valueOf(value);
+    setType(type);
+  }
+
+  public TaggedField(String name, UnitBase unit, Class<?> type) {
+    this(name, unit.getSymbol(), type);
+  }
+
+  public <T> void updateTag(T value) {
+    this.value = String.valueOf(value);
+    isNewValue = true;
+  }
+
+  @Override
+  public String getValue() {
+    String out = isNewValue ? value : "";
+    isNewValue = false;
+    return out;
+  }
+}
