e7392db3c2d128fddb1457354d320b8a2e11fa20
Hillel Coates <hillel.g.coates@gmail.com>
2019-12-28 17:44:42 -0800

    :fire: Remove JoystickTest and clean up Joystick

--- src/main/kotlin/org/frc1778/freezylib/util/Joystick.kt
+++ src/main/kotlin/org/frc1778/freezylib/util/Joystick.kt
@@ -3,23 +3,19 @@ package org.frc1778.freezylib.util
 import edu.wpi.first.wpilibj.DriverStation
 
 /**
- * Contains constants for The parent class of all joysticks, allowing for driver input to be easily
- * altered for different devices.
+ * Contains constants for The parent class of all joysticks, allowing for driver input to be easily altered for
+ * different devices.
  *
  * @author FRC 1778 Chill Out
  */
-class Joystick(private val name: String, private val port: Int) {
+object Joystick {
 
     fun isConnected(name: String, port: Int): Boolean {
-        return if (DriverStation.getInstance().isDSAttached) {
-            name == DriverStation.getInstance().getJoystickName(port) && (DriverStation.getInstance().getStickAxisCount(
-                port
-            ) > 0 || DriverStation.getInstance().getStickButtonCount(port) > 0 || DriverStation.getInstance().getStickPOVCount(
-                port
-            ) > 0)
-        } else {
-            false
-        }
+        return if (DriverStation.getInstance().isDSAttached)
+            name == DriverStation.getInstance().getJoystickName(port) &&
+            (DriverStation.getInstance().getStickAxisCount(port) > 0 ||
+            DriverStation.getInstance().getStickButtonCount(port) > 0 ||
+            DriverStation.getInstance().getStickPOVCount(port) > 0) else false
     }
 
     interface Base {
@@ -54,11 +50,11 @@ class Joystick(private val name: String, private val port: Int) {
         RIGHT_SHOULDER_SWITCH(2, 0),
         RESET_BUTTON(3, 0),
 
-        AXIS_LEFT_X(4, 1),
-        AXIS_LEFT_Y(2, 1),
-        AXIS_RIGHT_X(0, 1),
-        AXIS_RIGHT_Y(1, 1),
-        AXIS_TUNER(3, 1)
+        AXIS_LEFT_X(2, 1),
+        AXIS_LEFT_Y(5, 1),
+        AXIS_RIGHT_X(1, 1),
+        AXIS_RIGHT_Y(0, 1),
+        AXIS_TUNER(6, 1)
     }
 
     enum class FreezyController(override val id: Int, override val type: Byte) : Base {
